d963d92358bd4e4e98e11924e78a617ab9c408ff
Roland <sireanu.roland@gmail.com>
2021-03-01 14:12:40 +0200

    StringView

---
+++ C++_Ramp_up/StringView.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void printMe(string_view arg)
+{
+    cout << arg << endl;
+}
+
+int main()
+{
+    string s = "Hello world !";
+
+
+
+    return 0;
+}

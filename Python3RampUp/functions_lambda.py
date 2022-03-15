5fa541110df19ac695542581e5b1f02929bd0f36
RolandSireanu <34794070+RolandSireanu@users.noreply.github.com>
2022-03-15 12:19:15 +0200

    Python3 lambda functions

--- Python3RampUp/functions_lambda.py
+++ Python3RampUp/functions_lambda.py
@@ -17,3 +17,18 @@ t = [(7,"adsa") , (2,"wqe") , (0,"sapte")]
 
 print(sorted(t,key=lambda x : x[0]))
 
+# =================================================================================================
+
+anonFunc1 = lambda x,y: True if x>y else False
+anonFunc2 = lambda x,y: x>y
+#anonFunc1 and anonFunc2 are equivalents , lambda just returns the result of expression
+
+#multiple if else in the same lambda function, again lambda just evaluates the expression and returns the result
+anonFunc3 = lambda x,y: x-y if x > y else (y-x if y > x else x)
+
+if anonFunc1(5,2):
+    print("5 is bigger than 2!")
+
+print(anonFunc3(4,5))
+print(anonFunc3(5,4))
+print(anonFunc3(4,4))

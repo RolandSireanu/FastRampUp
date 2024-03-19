8b74accd0b05e43e720216004c1acf78bd476663
RolandSireanu <sireanu.roland@gmail.com>
2024-03-19 10:43:22 +0200

    C++ ref qualifiers

--- C++_Ramp_up/FunctionRefQualifiers.cpp
+++ C++_Ramp_up/FunctionRefQualifiers.cpp
@@ -1,78 +1,57 @@
 #include <iostream>
-#include <string>
-#include <vector>
+using namespace std;
 
-class Widget
+// =============== Ref qualified functions ==============
+// During overload resolution the compiler looks for the function &-qualified 
+// if the caller object is an lvalue or for the function &&-qualified if the caller object is an rvalue.
+// &:       The & qualifier after a member function declaration in C++ is called an "lvalue reference qualifier". This qualifier indicates that the function can only be called on lvalue expressions of the class
+// const&:  const:  This qualifier indicates that the member function does not modify any non-static member variables of the class. It can be called on both const and non-const instances of the class
+//          &:      Called only on lvalue objects
+//          const&: Doesn't modify the object and can be called only on lvalue expressions  
+// &&:      Rvalue reference qualifier, the function can only be called on rvalue expressions
+
+class Container
 {
-    public:
-        void p() &&         //Implementation for case when *this is a rvalue
-        {
-            std::cout << "Rvalue of this" << std::endl;
-        }
+public:
 
-        void p() &
-        {
-            std::cout << "Lvalue of this" << std::endl;
-        }
+    void begin() const& { std::cout << "const&" << std::endl;}
+    // void begin(const int*&)
+    // Can bind lvalue const/non-const "this" pointer: constantContainer.begin()
+    // Can bind rvalue "this" pointer: Container{}.begin()
+    
+    void begin() & {std::cout << "-" << std::endl; }
+    //  void begin(Container*&)
+    //  Can bind lvalue "this" pointers: myContainer.begin()
 
+    void begin() && {std::cout << "&&" << std::endl; }  
+    //  void begin(Container*&&)
+    //  Can bind only rvalue "this" pointers: Container{}.begin();
 
+    // void begin() const {}
+    // void begin(const Container*)
+    // Can bind const and non-const lvalue "this" pointer: constantContainer.begin()
 };
 
 
-// How function reference qualifiers are improving the code performance
-
-class MyData
-{
-
-    public:
-        std::vector<int>& data() &
-        {
-            std::cout << "Addr of v in MyData = " << &v << std::endl;
-            return v;
-        }
-
-        std::vector<int>&& data() &&
-        {
-            return std::move(v);
-        }
-
-
-    private:
-        std::vector<int> v;
-
-};
-
-MyData make_class()
-{
-    return MyData();
-}
-
-
+void f(int*& arg)       {}
+void f(const int*& arg) {}
+void f(int*&& arg)      {}
 
 int main()
 {
 
-    Widget w;
-    w.p();
-    Widget().p();
+    Container c;
+    Container const cc;
+    Container{}.begin();
 
-    MyData md;
-    //A tempo object MyData() is created , copied to vec2 , then destroyed
-    auto vec = md.data();       //Creates a copy of v in vec
+    c.begin();
+    cc.begin();
 
-    std::cout << "Addr vec = " << &vec << std::endl;
-
-
-    //We should have some option to specify that , when data() is invoked
-    //from and rvalue , the result should be an rvalue
-    // ===> Function reference qualifiers ! 
-
-    //No copies are done , just moving    
-    auto&& vec2 = make_class().data();
-    vec2.push_back(0x22);
-
-    
-
-
+    int a {1};
+    const int* ptr = &a;
+    int& ra = a;
+    const int& cra = a;
+    f(ptr);
+    f(new int);
     return 0;
 }

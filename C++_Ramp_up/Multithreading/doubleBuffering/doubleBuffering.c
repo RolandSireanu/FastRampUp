33a57f663b378c5b974608635d12a22489a41939
Sireanu Roland <roland@Sireanus-MacBook-Pro.local>
2024-11-23 11:08:23 +0200

    Explanations about multithreading pitfalls

--- C++_Ramp_up/Multithreading/doubleBuffering/doubleBuffering.c
+++ C++_Ramp_up/Multithreading/doubleBuffering/doubleBuffering.c
@@ -10,6 +10,29 @@
 #include <time.h>
 #include <stdlib.h>
 
+/*
+    Thread local storage (memory globally available in the contex of a single thread)
+    Memory isn't shared with other threads.
+    Each thread has a clear and isolated context, which enhances data integrity and clarity
+
+    Pitfalls in multithreaded programming:
+        Race conditions: Occurs when multiple threads access shared data and at least one of thme modifies it without a proper
+                         syncronization.
+                         At a fundamental level, the race condition reduces to "Read/Modify/Write" problem.
+                         Thread 1 reads value, Thread 2 modifies it, Thread 1 modifies it again => data is corrupted
+        
+        Deadlocks (deadly embrace): Happens when two threads are waiting for each other to release locks on the same mutex.
+                                    To avoid this, always ensure that in all functions executed by threads (like t1 and t2)
+                                    the program aquire locks in the same order.
+                                    Example:
+                                        void t1(void*){ pthread_mutex_lock(&lock1); ... pthread_mutex_lock(&lock2) ... }
+                                        void t1(void*){ pthread_mutex_lock(&lock2); ... pthread_mutex_lock(&lock1) ... }
+                                    
+
+
+*/
+
+
 #define NR_OF_THREADS       20
 #define MAX_NR_OF_ENTRIES   1024
 
@@ -42,15 +65,14 @@ pthread_cond_t  gCondSpaceInInput = PTHREAD_COND_INITIALIZER;
 pthread_cond_t gCondSpaceInOutput = PTHREAD_COND_INITIALIZER;
 pthread_cond_t gCondOutBufferAvailable = PTHREAD_COND_INITIALIZER;
 pthread_key_t gKey;
-
-int isPrime(int num) {
+static int isPrime(int num) {
     if (num <= 1) 
     {
         return 0;
     }
     if (num <= 3) 
     {
-        return 1; // 2 and 3 are prime numbers
+        return 1;
     }
 
     for(int i=2; i<num; ++i)
@@ -64,7 +86,7 @@ int isPrime(int num) {
     return 1;
 }
 
-int SumPrimeNumbers() 
+static int SumPrimeNumbers() 
 {
     int sum = 0;
     Request_t lReq = *((Request_t*)pthread_getspecific(gKey));
@@ -79,34 +101,47 @@ int SumPrimeNumbers()
     return sum;
 }
 
-int inline GenEndInterval()
+static int inline GenEndInterval()
 {
-    return rand() % 100000;
+    return rand() % 10000;
 }
 
-void AddToOutpBuffer(int aResp)
+void Destruct(void* arg)
 {
-    pthread_mutex_lock(&gOutBuffer.mMutex);
-    // If first free position is < MAX_NR_OF_ENTRIES => there is still space in gOutBuffer
+    printf("Destruct called\n");
+    free(arg);
+}
+
+static void AddToOutpBuffer(int aResp)
+{
+    pthread_mutex_lock(&gOutBuffer.mMutex);    
     while(gOutBuffer.mNrOfEntries == MAX_NR_OF_ENTRIES)
     {
+        // A condition variable is just a syncronization point between threads.
+        // Threads are put on sleep until a specific signal come
+        // Condition variable often leverage futexes (mutex in user space)
+        // Why are condition variables important? They eliminate the constant pooling issue.
         pthread_cond_wait(&gCondSpaceInOutput, &gOutBuffer.mMutex);
     }
     
-    // Add aResp to gOutBuffer on the first free position    
     if(gOutBuffer.mFirstFreePos < MAX_NR_OF_ENTRIES-1)
     {
-        gOutBuffer.mBuffer[gOutBuffer.mFirstFreePos] = aResp;
-        // Increment next free position in gOutBuffer
+        gOutBuffer.mBuffer[gOutBuffer.mFirstFreePos] = aResp;    
         gOutBuffer.mFirstFreePos++;
         gOutBuffer.mNrOfEntries++;
     }
     
+    /*
+        Why would I call pthread_cond_signal when pthread_cond_broadcast is a superset of the first one?
+            - Express intent clearer, improve readability and maintainability of your code
+            - Avoid unnecessary wake-ups, when all threads are awakened, they all try to reaquire the mutex
+              which leads to context switching and unnecessary overhead.
+    */
     pthread_cond_broadcast(&gCondOutBufferAvailable);
     pthread_mutex_unlock(&gOutBuffer.mMutex);
 }
 
-int GetFromOutBuffer()
+static int GetFromOutBuffer()
 {
     int lResp;
 
@@ -127,13 +162,11 @@ int GetFromOutBuffer()
     return lResp;
 }
 
-void AddToInputBuffer(Request_t aReq)
+static void AddToInputBuffer(Request_t aReq)
 {
-    pthread_mutex_lock(&gInBuffer.mMutex);
-    // If first free position in buffer is < MAX_NR_OF_ENTRIES => there is still space in gOutBuffer
+    pthread_mutex_lock(&gInBuffer.mMutex);    
     while(gInBuffer.mNrOfEntries == MAX_NR_OF_ENTRIES)
     {
-        // If buffer is full => wait in SpaceInInput condition
         pthread_cond_wait(&gCondSpaceInInput, &gInBuffer.mMutex);
     }
     
@@ -143,13 +176,12 @@ void AddToInputBuffer(Request_t aReq)
         gInBuffer.mFirstFreePos++;
         gInBuffer.mNrOfEntries++;
     }
-    
-    printf("Nr of entries in input buffer %d\n", gInBuffer.mNrOfEntries);
+        
     pthread_cond_broadcast(&gCondInputAvailable);
     pthread_mutex_unlock(&gInBuffer.mMutex);
 }
 
-Request_t GetFromInBuffer()
+static Request_t GetFromInBuffer()
 {
     Request_t lReq;
 
@@ -174,25 +206,29 @@ Request_t GetFromInBuffer()
 
 void* Thread(void* arg)
 {
+    Request_t* lPtrReq = (Request_t*)malloc(sizeof(Request_t));
     while(1)
     {   
-        Request_t lReq = GetFromInBuffer();
+        Request_t lReq = GetFromInBuffer();        
+        *lPtrReq = lReq;
 
-        pthread_setspecific(gKey, &lReq);
+        pthread_setspecific(gKey, lPtrReq);
         int lResp = SumPrimeNumbers();
-        printf("Start/EndInterval = [%d - %d]   SumPrimeNumbers = %d\n", lReq.mStartInterval, lReq.mEndInterval, lResp);
-        AddToOutpBuffer(lResp);
+        // printf("Start/EndInterval = [%d - %d]   SumPrimeNumbers = %d\n", lReq.mStartInterval, lReq.mEndInterval, lResp);
+        AddToOutpBuffer(lResp);        
     }
-    return NULL;
+
+    pthread_exit(NULL);
 }
 
 int main()
 {
     int lId = 0;
     pthread_t lArrayOfThreads[NR_OF_THREADS];
-    srand(time(NULL));   // Initialization, should only be called once
+    srand(time(NULL));
 
-    pthread_key_create(&gKey, NULL);
+    // Whenever a thread exits "Destruct"" function is called for each data that was set with "pthread_setspecific"
+    pthread_key_create(&gKey, Destruct);
 
     for(int i=0; i<MAX_NR_OF_ENTRIES; ++i)
     {
@@ -202,7 +238,8 @@ int main()
 
     for(int i=0; i<NR_OF_THREADS; ++i)
     {
-        Request_t lTempReq = {.mStartInterval=0, .mEndInterval = GenEndInterval()};
+        int lEndInterval = GenEndInterval();
+        Request_t lTempReq = {.mStartInterval=0, .mEndInterval = lEndInterval};
         lId++;
         AddToInputBuffer(lTempReq);
     }
@@ -216,17 +253,19 @@ int main()
     {
         for(int i=0; i<NR_OF_THREADS; ++i)
         {
-            Request_t lTempReq = {.mStartInterval=0, .mEndInterval = GenEndInterval()};
+            int lEndInterval = GenEndInterval();
+            Request_t lTempReq = {.mStartInterval=0, .mEndInterval = lEndInterval};
             lId++;
             AddToInputBuffer(lTempReq);    
-
         }
-
-        int lResponse = GetFromOutBuffer();
+        
+        printf("Response sum = %d\n", GetFromOutBuffer());
     }
 
     for(int i=0; i<NR_OF_THREADS; ++i)
     {
         pthread_join(lArrayOfThreads[i], NULL);
     }
+
+    pthread_key_delete(gKey);    
 }

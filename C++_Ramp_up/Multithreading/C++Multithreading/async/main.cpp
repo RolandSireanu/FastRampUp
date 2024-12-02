2ec46a942b5fb0654d07616813ac5b0b37b6bc88
Sireanu Roland <sireanu.roland@gmail.com>
2024-12-02 14:19:06 +0200

    C++ async update

---
+++ C++_Ramp_up/Multithreading/C++Multithreading/async/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <future>
+#include <thread>
+#include <chrono>
+
+
+bool readFromIO()
+{
+    int counter {0};
+
+    while(counter < 1000)
+    {
+        std::cout << "Reading from IO ... counter = " << counter << "\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        counter += 50;
+    }
+
+    return true;
+}
+
+int main()
+{
+    std::future<bool> backgroundThread = std::async(std::launch::async, readFromIO);
+    std::future_status status;
+    while(true)
+    {
+        std::cout << "Main thread running ... \n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        status = backgroundThread.wait_for(std::chrono::milliseconds(1));
+
+        if(status == std::future_status::ready)
+        {
+            std::cout << "Data ready! \n";
+            break;
+        }
+    }
+
+    std::cout << "Exit main thread \n";
+
+
+    return 0;
+}

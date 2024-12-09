1ffa6523656cc5b017fb21fadf9e4b1b719d1e98
Sireanu Roland <sireanu.roland@gmail.com>
2024-12-09 23:59:39 +0200

    C++ MyVector update

--- C++_Ramp_up/MyVector/include/MyVector.hpp
+++ C++_Ramp_up/MyVector/include/MyVector.hpp
@@ -5,6 +5,37 @@
 template<typename T, typename Allocator = std::allocator<T>>
 class MyVector
 {
+    using value_type =              T;
+    using allocator_type =          Allocator;
+    using size_type =               std::size_t;
+    using difference_type =         std::ptrdiff_t;
+    using reference =               value_type&;
+    using const_reference =         const reference&;
+    using pointer =                 value_type*;
+    using const_pointer =           const value_type*;
+
+public:
+
+    explicit MyVector(size_type aNrOfElements) {}
+    MyVector(size_type aNrOfElements, const_reference aValue) {}
+    explicit MyVector(std::initializer_list<T> aInitList) {}
+    
+    template<typename InputIt>
+    MyVector(InputIt aFirst, InputIt aLast) {}
+
+    MyVector() noexcept : MyVector(Allocator()) {}
+    MyVector(const MyVector& aVector) = default;
+    MyVector(MyVector&& aVector) = default;
+    MyVector& operator=(const MyVector& aVector) = default;
+    MyVector& operator=(MyVector&& aVector) = default;
+    MyVector& operator=(std::initializer_list<T> aInitList) {}
+    
+    explicit MyVector(const Allocator& aAllocator = Allocator()) {}
+
+    void assign(size_type aNrOfElements, const_reference aValue) {}
+    template<typename InputIt>
+    void assign(InputIt aFirst, InputIt aLast) {}
+    void assign(std::initializer_list<T> aInitList) {}
 
 };
 

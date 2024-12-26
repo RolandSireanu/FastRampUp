3a3729c7c3ed0badeec54b644ca40f2029162b9c
RolandSireanu <sireanu.roland@gmail.com>
2024-12-26 23:51:26 +0200

    C++ flat_map benchmark

--- C++_Ramp_up/GBenchmark/src/dummy_benchmark.cpp
+++ C++_Ramp_up/GBenchmark/src/dummy_benchmark.cpp
@@ -1,10 +1,45 @@
 #include <benchmark/benchmark.h>
+#include <boost/container/flat_map.hpp>
+#include <unordered_map>
 
-static void BM_Dummy(benchmark::State& state) {
-  for (auto _ : state) {
-    // Dummy benchmark code
+template <typename T>
+class Map : public benchmark::Fixture {
+public:
+  void SetUp(::benchmark::State& state) {
+    // Create a boost flat_map and insert 1000 random elements
+    for (int i = 0; i < mSize; i++) {
+      mMap.insert({i, rand()});
+    }
+  }
+
+  void TearDown(::benchmark::State& state) {
+  }
+
+  const int32_t mSize {1000000};
+  const int32_t mStep {100};
+  T mMap;
+
+};
+
+// Defines and registers `FooTest` using the class `MyFixture`.
+BENCHMARK_TEMPLATE_F(Map, FlatMapBenchmark, boost::container::flat_map<int,int>)(benchmark::State& st) {
+   for (auto _ : st) {
+    for(int32_t i{0}; i<mSize; ++i)
+    {
+      int32_t lIndex {rand() % mSize};
+      (void)mMap.find(lIndex);
+    }
   }
 }
-BENCHMARK(BM_Dummy);
+
+BENCHMARK_TEMPLATE_F(Map, UnorderedMapBenchmark, std::unordered_map<int,int>)(benchmark::State& st) {
+   for (auto _ : st) {
+    for(int32_t i{0}; i<mSize; ++i)
+    {
+      int32_t lIndex {rand() % mSize};
+      (void)mMap.find(lIndex);
+    }
+  }
+}
 
 BENCHMARK_MAIN();

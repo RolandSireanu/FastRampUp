d8c72e6e133319f47205939c5c73602d0b161bb1
Sireanu Roland <sireanu.roland@gmail.com>
2025-01-26 15:05:33 +0200

    C++ No false sharing

--- C++_Ramp_up/FalseSharingBenchmark/src/false_sharing.cpp
+++ C++_Ramp_up/FalseSharingBenchmark/src/false_sharing.cpp
@@ -1,27 +1,61 @@
 #include <benchmark/benchmark.h>
-#include <unordered_map>
-#include <map>
+#include <vector>
+#include <array>
 #include <iostream>
 #include <thread>
-#include <utility>
+#include <numeric>
+#include <atomic>
+#include <cassert>
 
-template<int32_t D>
+struct alignas(64) AlignedInt32 {
+  int32_t value;
+};
+
+const size_t lMaxNoThreads {std::thread::hardware_concurrency()};
+std::vector<AlignedInt32> lAcc(lMaxNoThreads);
+std::array<int32_t, 1024*64> lInData;
+
+template<auto D>
 void work(const std::array<int32_t, D>& aData)
 {
-  const size_t lMaxNoThreads {std::thread::hardware_concurrency()};
-  std::array<std::jthread, lMaxNoThreads> lThreads;
+  constexpr int32_t lExpectedResult = (((D-1) * D) / 2);
+  std::vector<std::jthread> lThreads;
+  lThreads.reserve(lMaxNoThreads);
+
+  for(int32_t i{0}; i<lMaxNoThreads; ++i)
+  {
+    lThreads.emplace_back(std::jthread([&aData, i](){
+      for(const auto& e : aData)
+      {
+        lAcc[i].value += e;
+      }
+    }));
+  }
+
+  for(int32_t i{0}; i<lMaxNoThreads; ++i)
+  {
+    lThreads[i].join();
+  }
+
+  for(const auto& e : lAcc)
+  {
+    if(e.value != lExpectedResult)
+      throw std::runtime_error("Error: incorrect result");
+  }
 }
+
 
 static void BM_FalseSharing(benchmark::State& state) 
 {
-
-
+  std::iota(lInData.begin(), lInData.end(), 0);
 
   for (auto _ : state) 
-  {}
+  {
+    work(lInData);
+  }
 }
 
 
-BENCHMARK(BM_FalseSharing)->UseRealTime()->Unit(benchmark::kMillisecond)->Iterations(1000);
+BENCHMARK(BM_FalseSharing)->UseRealTime()->Unit(benchmark::kMillisecond)->Iterations(1);
 
 BENCHMARK_MAIN();
